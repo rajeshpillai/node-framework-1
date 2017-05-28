740a79622b637f1c26a955bc7ff1a315250e16bb
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-05-28 09:07:09 +0530

    todo: Nested routes: comments eg

--- app/index.js
+++ app/index.js
@@ -3,7 +3,11 @@ var posts = [
     id: 1,
     permalink: 'about',
     title: 'About Learn by building a front end framework',
-    content: "Master JS by buidling a front end framework from scratch."
+    content: "Master JS by buidling a front end framework from scratch.",
+    comments: [
+      { id: 1, comment: "Can you please explain in detail the popstate?", postedBy: "learner"},
+      { id: 1, comment: "Sure I will", postedBy: "rajesh"},
+    ]
   },
   {
     id: 2,
@@ -18,5 +22,6 @@ var router = new Router();
 router.map(function () {
     this.route("posts", { path: '/posts'});
     this.route("post", { path: '/posts/:permalink'});
+    this.route("comments", { path: '/posts/:permalink/comments'});
     this.route("favorites", { path: '/favs'});
 });
